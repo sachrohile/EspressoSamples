031002063ea9259243795e024584fa04925ff575
sachin.shear <sachin.shelar@ecotechservices.com>
2018-09-06 22:59:44 +0530

    Add more basic tests

--- app/src/androidTest/java/com/thesachlab/espressosamples/ui/basic/BasicActivityTest.java
+++ app/src/androidTest/java/com/thesachlab/espressosamples/ui/basic/BasicActivityTest.java
@@ -14,9 +14,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -40,7 +42,9 @@ public class BasicActivityTest {
 
     @Test
     public void enterValueInTextView(){
+        onView(withId(R.id.edtBasic)).check(matches(isDisplayed()));
         onView(withId(R.id.edtBasic)).perform(typeText("BasicText"));
+        onView(withId(R.id.edtBasic)).perform(clearText());
     }
 
     @Test
